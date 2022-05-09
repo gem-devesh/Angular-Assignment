b7905d5012105ad65740324c966defbfe0757d6a
gem-devesh <devesh.singh@geminisolutions.com>
2022-05-09 11:21:16 +0530

    Addded template form

--- src/app/template/template.component.ts
+++ src/app/template/template.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-template',
@@ -11,5 +13,51 @@ export class TemplateComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  isSubmitted = false;
+  isCourseNameCorrect: Boolean = true;
+  isCourseDescCorrect: Boolean = true;
+  isCourseAmountCorrect: Boolean = true;
+  isSubmit = false
 
+  onSubmit(form: NgForm) {
+    this.isSubmit = true;
+    if (!/[^a-zA-Z]/.test(form.value.courseName)) {
+      this.isCourseNameCorrect = false;
+    }
+
+    if (!/[^a-zA-Z]/.test(form.value.courseDesc)) {
+      this.isCourseDescCorrect = false;
+    }
+
+    if (!/[^0-9]/.test(form.value.courseAmount)) {
+      this.isCourseAmountCorrect = false;
+    }
+
+    console.log(this.isCourseAmountCorrect);
+    console.log(this.isCourseDescCorrect);
+    console.log(this.isCourseNameCorrect);
+
+
+
+    if (!this.isCourseAmountCorrect && !this.isCourseDescCorrect && !this.isCourseNameCorrect) {
+      this.isSubmitted = true;
+      let JSONData = [
+        { "courseName": form.value.courseName },
+        { "courseDesc": form.value.courseDesc },
+        { "courseAmount": form.value.courseAmount }
+      ]
+
+      localStorage.setItem("templateData", JSON.stringify(JSONData));
+
+      let data = localStorage.getItem('templateData');
+      data = JSON.parse('templateData');
+
+      console.log(data);
+
+    }
+  }
+
+  onClear() {
+
+  }
 }
