befce000c2b9128adb5824a5709bb49feef95c07
gem-devesh <devesh.singh@geminisolutions.com>
2022-05-09 10:21:23 +0530

    Added Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveComponent } from './reactive/reactive.component';
+import { TemplateComponent } from './template/template.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveComponent,
+    TemplateComponent
   ],
   imports: [
     BrowserModule,
